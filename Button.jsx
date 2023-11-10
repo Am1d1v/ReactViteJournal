0fa72eb647d4dabf206e0530ca387f6e8457993c
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-10 15:49:01 +0300

    Add files via upload

--- Button.jsx
+++ Button.jsx
@@ -1,14 +1,21 @@
+import { useState } from 'react'
 import './Button.css'
 
 
 const Button = () => {
 
+    const [btnText, setBtnText] = useState('Open');
+
     const clicked = () => {
+        
+        setBtnText((text) => {
+            return text + ' !' // Previous state + ' !'
+        })
         console.log('Clicked')
     }
 
     return (
-        <button className='button accent' onClick={clicked}>Click</button>
+        <button className='button accent' onClick={clicked}>{btnText}</button>
     )
 }
 export default Button;
