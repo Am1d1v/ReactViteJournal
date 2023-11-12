1c233f7441dbdd69315f639ac99f811c0f6903dd
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-12 15:00:14 +0300

    Add files via upload

--- JournalForm.jsx
+++ JournalForm.jsx
@@ -4,9 +4,16 @@ import Button from '../Button/Button';
 
 const JournalForm = ({onSubmit}) => {
 
+  // Text Input 
   const [inputData, setInputData] = useState('');
 
-    // inputData change state  / Text Input
+    // Checking form validation
+    const [formValidState, setFormValidState] = useState({
+      title: true,
+      text: true, 
+    });
+
+    // inputData change state  / Text Input change state
     const inputChange = (event) => {
     
       setInputData(event.target.value);
@@ -26,16 +33,54 @@ const JournalForm = ({onSubmit}) => {
         // Invalid time value fix
         //console.log(formProps.date === '');
         formProps.date === '' ? formProps.date = new Date() : new Date()
-        onSubmit(formProps);
 
+        // Checking form validation
+        let isFormValid = true;
+
+        if(!formProps.title.trim().length){
+          setFormValidState(state => ({...state, title: false}));
+          isFormValid = false;
+        } else {
+          setFormValidState(state => ({...state, title: true}));
+        }
+
+        if(!formProps.text.trim().length){
+          setFormValidState(state => ({...state, text: false}));
+          isFormValid = false;
+        } else {
+          setFormValidState(state => ({...state, text: true}));
+        }
+
+        // If isFormValid false(title or text is missing), user cannot submit his note 
+        if(!isFormValid){
+          return;
+        }
+
+        onSubmit(formProps);
     }
 
     return (
             <form className='journal-form' onSubmit={addJournalItem}>
-                <input type='text' name='title' placeholder='Title'/>
-                <input type='date' name='date' />
-                <input type='text' name='tag' placeholder='Text' value={inputData} onChange={inputChange} />
-                <textarea name="text" placeholder='Description' id="" cols="30" rows="10"></textarea>
+                <input type='text' 
+                       name='title' 
+                       placeholder='Title' 
+                       style={{border: formValidState.title ? 'none' : '2px solid red'}}
+                       />
+                <input type='date' 
+                       name='date' />
+                <input type='text' 
+                       name='tag' 
+                       placeholder='Text' 
+                       value={inputData} onChange={inputChange}
+                       style={{border: formValidState.text ? 'none' : '2px solid red'}} 
+                       />
+                <textarea name="text" 
+                          placeholder='Description' 
+                          id="" 
+                          cols="30" 
+                          rows="10"
+                          style={{border: formValidState.text ? 'none' : '2px solid red'}}
+                          ></textarea>
                 <Button text='Save' />
             </form>  
     )
