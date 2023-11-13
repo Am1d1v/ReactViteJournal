01155d91b3ceed0fcdc3699e10f6850dba7b80d5
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-13 16:30:03 +0300

    Add files via upload
    state useEffect

--- JournalForm.jsx
+++ JournalForm.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 
 const JournalForm = ({onSubmit}) => {
 
-  // Text Input 
-  const [inputData, setInputData] = useState('');
+    // Text Input 
+    const [inputData, setInputData] = useState('');
 
-    // Checking form validation
-    const [formValidState, setFormValidState] = useState({
+    // Initial Data State
+    const INITIAL_STATE = {
       title: true,
       post: true, 
-    });
+    };
+
+    // Checking form validation
+    const [formValidState, setFormValidState] = useState(INITIAL_STATE);
 
     // inputData change state  / Text Input change state
     const inputChange = (event) => {
@@ -19,6 +22,15 @@ const JournalForm = ({onSubmit}) => {
       setInputData(event.target.value);
       //console.log(inputData)
     }
+
+    // Changing post(description) or title state after 2 seconds
+    useEffect(() => {
+      if(!formValidState.post || !formValidState.title){
+        setTimeout(() => {
+          setFormValidState(INITIAL_STATE)
+        }, 2000);
+      }
+    }, [formValidState]);
 
     // Add New Journal Record
     const addJournalItem = (e) => {
@@ -31,10 +43,10 @@ const JournalForm = ({onSubmit}) => {
         //console.log(formProps);
 
         // Invalid time value fix
-        //console.log(formProps.date === '');
-        // const date = formProps.date;
-        // const formatedDate =  new Intl.DateTimeFormat('ru-RU').format(date);
-        // formProps.date === '' ? formProps.date = new Date() : new Date()
+          //console.log(formProps.date === '');
+          // const date = formProps.date;
+          // const formatedDate =  new Intl.DateTimeFormat('ru-RU').format(date);
+          // formProps.date === '' ? formProps.date = new Date() : new Date()
 
         const date = new Intl.DateTimeFormat('ru-RU').format(new Date());
         formProps.date === '' ? formProps.date = date : date
