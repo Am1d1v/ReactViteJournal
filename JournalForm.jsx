e2e4305facf4245e347a7fb7c33d09d641d8dddd
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-11-13 21:55:45 +0300

    Add files via upload

--- JournalForm.jsx
+++ JournalForm.jsx
@@ -11,14 +11,6 @@ const JournalForm = ({onSubmit}) => {
     // Text Input 
     const [inputData, setInputData] = useState('');
 
-    // Initial Data State
-    // const INITIAL_STATE = {
-    //   title: true,
-    //   post: true, 
-    // };
-    // Checking form validation
-    //const [formValidState, setFormValidState] = useState(INITIAL_STATE);
-
     // inputData change state  / Text Input change state
     const inputChange = (event) => {
     
@@ -42,6 +34,13 @@ const JournalForm = ({onSubmit}) => {
     }, [isValid]);
 
 
+    // useEffect for isFormReadyToSubmit
+    useEffect(() => {
+      if(isFormReadyToSubmit){
+        onSubmit(values)
+      }
+    }, [isFormReadyToSubmit])
+
     // Add New Journal Record
     const addJournalItem = (e) => {
         e.preventDefault();
@@ -50,52 +49,13 @@ const JournalForm = ({onSubmit}) => {
 
         // Getting Entries from fromData
         const formProps = Object.fromEntries(formData);
-        //console.log(formProps);
-
-        // Invalid time value fix
-          //console.log(formProps.date === '');
-          // const date = formProps.date;
-          // const formatedDate =  new Intl.DateTimeFormat('ru-RU').format(date);
-          // formProps.date === '' ? formProps.date = new Date() : new Date()
-
+        
         const date = new Intl.DateTimeFormat('ru-RU').format(new Date());
         formProps.date === '' ? formProps.date = date : date
-        //console.log(formProps.date, formProps)
-
-        // Checking form validation
-        //let isFormValid = true;
-
-        // if(!formProps.title.trim().length){
-        //   setFormValidState(state => ({...state, title: false}));
-        //   formState.isValid.title = false;
-        // } else {
-        //   setFormValidState(state => ({...state, title: true}));
-        // }
-
-        // if(!formProps.post.trim().length){
-        //   setFormValidState(state => ({...state, post: false}));
-        //   formState.isValid.post = false;
-        // } else {
-        //   setFormValidState(state => ({...state, post: true}));
-        // }
-
-        // // If isFormValid false(title or text is missing), user cannot submit his note 
-        // if(!isFormValid){
-        //   return;
-        // }
-
-        // useEffect for isFormReadyToSubmit
-          useEffect(() => {
-            if(isFormReadyToSubmit){
-              onSubmit(values)
-            }
-          }, [isFormReadyToSubmit])
-
 
         // Dispatch call   
         dispatchForm({type: 'SUBMIT', payload: formProps})
 
-        //onSubmit(formProps);
     }
 
     return (
